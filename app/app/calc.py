65691df6d0dda9d984da7ef8f56b9b870aae1ad7
Nicolas <nicolasmarcope@gmail.com>
2023-06-23 23:05:32 +0200

    Configured database and wait_for_db

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator FUnctions
+"""
+
+
+def add(x, y):
+    """Add X anf Y And Return result"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract X from Y and return result"""
+    return y - x
